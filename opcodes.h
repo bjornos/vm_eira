722cb0e40c66b809451045fcc6f4dd9bf58768bd
Björn Östby <bjorn.ostby@gmail.com>
2017-02-07 22:21:42 +0100

    opcodes: update documentation

--- opcodes.h
+++ opcodes.h
@@ -1,95 +1,148 @@
-/*
- * opcodes.h
- *
- *  Created on: Jan 17, 2017
- *      Author: bjornos
- */
-
 #ifndef OPCODES_H_
 #define OPCODES_H_
 
 #define PROGRAM_MAGIC 0xdcbafedc
 
-/* instruction: halt
- * | 0000 0000 | 0000 0000 | 0000 0000 0000 0000 |
- *   0           8           16
+/**
+ * instruction: halt
+ *
+ * | 0000 0000 | 0000 0000 0000 0000 0000 0000 |
+ * 0           8                               31
+ *                         reserved
  */
 #define halt	0x00
 
-/* instruction: nop
- * | 1000 0000 | 0000 0000 | 0000 0000 0000 0000 |
- *   0           8           16
+/**
+ * instruction: nop
+ *
+ * | 1000 0000 | 0000 0000 0000 0000 0000 0000 |
+ * 0           8                               31
+ *                        reserved
  */
 #define nop		0x01
 
-/* instruction: add [dst, src]
- * | 0100 0000 | 0000      | 00    00      |  0000 0000 0000 0000 |
- *   0          8 [DSTREG]  12src 14dst     16 [VALUE | SRC ADDR | REG] | DST ADDR ]
- *                              0: val
- *                              1: reg
- *                              2: mem
+/**
+ * instruction: add
+ *
+ * syntax: add dest, source
+ *
+ * | 0100 0000 | 0000 | 00  | 00 | 0000 0000 0000 0000 |
+ * 0           8      12    14   16                    31
+ *               reg  src    dst    value/memory/reg
+ *                    type   type
+ *
+ * if destination is memory, the address is stored in the upper 16 bits and
+ * the value is taken from registerX in bits 8-12. It is NOT possible to add
+ * immediate values directly to memory.
+ *
+ * example usage:
+ *  add r0,16   - r0 = r0 + 16
+ *
+ *  mov r1, 16
+ *  add @128, r1 - ram[128] = ram[128] + 16
+ *
  */
 #define add		0x02
 
 
-
+/**
+ * instruction: sub
+ *
+ * syntax: sub dest, source
+ *
+ * | 1100 0000 | 0000 | 00  | 00 | 0000 0000 0000 0000 |
+ * 0           8      12    14   16                    31
+ *                reg  src    dst    value/memory/reg
+ *                     type   type
+ *
+ * if destination is memory, the address is stored in the upper 16 bits and
+ * the value is taken from registerX in bits 8-12. It is NOT possible to sub
+ * immediate values directly to memory.
+ *
+ * example usage:
+ *  sub r0,16   - r0 = r0 + 16
+ *
+ *  mov r1, 28
+ *  sub @128, r1 - ram[128] = ram[128] - 28
+ *
+ */
 #define sub		0x03
 
-/** instruction: jmp [label]
- * | 0000 0000 | 0000 0000 0000 0000 0000 0000 |
- *   0          8      address     24| reserved
+/**
+ * instruction: mov
+ *
+ * syntax: mov dest, source
+ *
+ *  | 0010 0000 | 0000 | 00  | 00 | 0000 0000 0000 0000 |
+ *  0           8      12    14   16                    31
+ *                 reg  src    dst    value/memory/reg
+ *                      type   type
+ *
+ * if destination is memory, the address is stored in the upper 16 bits and
+ * the value is taken from registerX in bits 8-12. It is NOT possible to move
+ * immediate values directly to memory.
+ *
+ * example usage:
+ *  mov r0,16   - general purpose register 0 = 16
+ *
+ *  mov r10, 60
+ *  mov @128, r10 - ram[128] = 60
+ *
+ */
+#define mov		0x04
+
+
+/**
+ * instruction: jmp
+ *
+ * syntax: jmp [label]
+ *
+ * the assembler/compiler need to determine the conversion from
+ * [label] to address.
+ *
+ *  | 0000 1000 | 0000 0000 0000 0000 | 0000 0000 |
+ *  0           8                     24          31
+ *                     pc address        reserved
 */
 #define jmp		0x10
 
-/** instruction: cmp
+/**
+ * instruction: cmp
  *
- *  syntax
- *  cmp [gp reg],[value]
+ * syntax: cmp [gp reg],[value]
  *
- *  The result is placed in the cpu conditional register
+ * compare the value in general purpose register [reg] with immediate
+ * value. the result is placed in the cpu conditional register (cr)
  *
- * | 0001 0001 |  0000  | 0000 | 0000 0000 0000 0000 |
- *   0          8       12      16
+ * TODO: add compare from memory or other register
+ *
+ * | 1001 1000 |  0000  | 0000 | 0000 0000 0000 0000 |
+ * 0           8        12     16
  *    instr       reg     src          value
 */
 #define cmp		0x11
 
-/** instruction: breq
+/**
+ * instruction: branch equal
  *
- *  syntax
- *  breq [address]
- *  jump to address if compare result was equal
+ * syntax: breq [address]
  *
- *  desc
+ * jump to address if compare result in conditional register is COND_EQ
  *
  * | 0001 0010 | 0000 0000 | 0000 0000 0000 0000 |
- *   0          8            16
- *    instr       reserved   addr
+ * 0           8           16
+ *    instr       reserved           addr
 */
 #define breq	0x12
 
 
 
-/* instruction: mov [dst, src]
- * | 0000 0000 | 0000      | 00    00      |  0000 0000 0000 0000 |
- *   0          8 [DSTREG]  12src 14dst     16 [VALUE | ADDR | REG]
- *                              0: val
- *                              1: reg
- *                              2: mem
- * add r0,@0xfea (addr)
- * add r1,r2
- * add @1024, 255
- * add @512, r3
- * add r1,0xffa    0x02 00 01 00 0000 ffa0 0000
- * add r1,0xff     0x02 00 01 00 0000 ff00 0000 0xff0102
+
+/*
+ * instructions below are experimental
+ * -----------------------------------
  *
- * dest           src
- * mem reg val   mem reg val
- *
- *
- * general purpose register = 16 st. -> 4 bitar.
-*/
-#define mov		0x04
+ */
 
 /* no args */
 #define clrscr	0xf0
@@ -129,35 +182,5 @@
  *   The result must be placed in general purpose registers
  */
 #define screenout	0xf3
-
-
-
-
-/*   |-------| 255 - 8 bitars, 16 bitars maxint.
- 0000 0000 0000 0000
-
-mov src, dst
-
-mov 100, r2
-mov @100, r4
-
-
-instr reg  imm val/mem addr
-|---|----|----|----|
-0000 0000 0000 0000
-
-
-255 instr.
-16 reg
-65535 minne
-
-uint32_t
-instr      reg reg     imm val memaddr
-|---|----|----|----| |---------------|
-0000 0000 0000 0000  00000000 00000000
-
-
-
-*/
 
 #endif /* OPCODES_H_ */
